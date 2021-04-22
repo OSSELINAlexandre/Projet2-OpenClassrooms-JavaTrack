38b7b9d176968e2c195948c2db7ab8dc5cb47693
Alexandre Osselin <alexandre.osselin@gmail.com>
2021-04-22 12:37:50 +0200

    First error : not class variable but local variable used in the code leading to error

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,39 +1,48 @@
 package com.hemebiotech.analytics;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class AnalyticsCounter {
-	private static int headacheCount = 0;	// initialize to 0
+	
+	private static int headacheCount = 0;	// initialize to 0 First mistake : The variable used in the code is different from the
+	// class variable written here. 
 	private static int rashCount = 0;		// initialize to 0
 	private static int pupilCount = 0;		// initialize to 0
 	
 	public static void main(String args[]) throws Exception {
-		// first get input
-		BufferedReader reader = new BufferedReader (new FileReader("C:\\Users\\FamilleOSSELIN\\git\\Project_DA_Java_EN_Come_to_the_Rescue_of_a_Java_Application\\Project02Eclipse\\symptoms.txt"));
+		
+		String currentPath = System.getProperty("user.dir");
+		String symptomPath = currentPath + "\\Project02Eclipse\\symptoms.txt" ;
+		FileReader file = new FileReader(symptomPath);
+		BufferedReader reader = new BufferedReader(file);
 		String line = reader.readLine();
-		System.out.println("HEY BUDDY WE ARE TESTING");
 		
 		int i = 0;	// set i to 0
-		int headCount = 0;	// counts headaches
+		//  headaches = 0; counts headaches !! Yes, but you do have a global variable for that Purpose
 		while (line != null) {
 			i++;	// increment i
-			System.out.println("symptom from file: " + line);
+			System.out.println("symptom n°" + i + " from file: " + line);
 			if (line.equals("headache")) {
-				headCount++;
-				System.out.println("number of headaches: " + headCount);
+				headacheCount++;
+				System.out.println("number of headaches: " + headacheCount);
 			}
-			else if (line.equals("rush")) {
+			else if (line.equals("rush")) { 
 				rashCount++;
+				System.out.println("number of rush: " + rashCount);
 			}
 			else if (line.contains("pupils")) {
 				pupilCount++;
+				System.out.println("number of pupilsache: " + pupilCount);
 			}
 
 			line = reader.readLine();	// get another symptom
 		}
-		
+				
 		// next generate output
 		FileWriter writer = new FileWriter ("result.out");
 		writer.write("headache: " + headacheCount + "\n");
