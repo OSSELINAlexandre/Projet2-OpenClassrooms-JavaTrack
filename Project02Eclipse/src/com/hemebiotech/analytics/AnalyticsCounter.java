2d9148dc683f0fedf9112be861a9343327e2a1b5
Alexandre Osselin <alexandre.osselin@gmail.com>
2021-04-29 15:14:47 +0200

    Version 1.1 of my Solution

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -6,48 +6,87 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.TreeMap;
+
+/**
+ * <b>A solution</b>
+ * <p>
+ * What changed ?
+ * <ul>
+ * <li>The symptoms are into a Dictionary</li>
+ * <li>To obtain the result you want in the output file, you just have to call
+ * the method in ReadSymptomDataFromFile</li>
+ * </ul>
+ * </p>
+ * 
+ * @see ReadSymptomDataFromFile
+ * 
+ * @author Alexandre OSSELIN
+ * @version 1.1
+ */
 
 public class AnalyticsCounter {
-	
-	private static int headacheCount = 0;	// initialize to 0 First mistake : The variable used in the code is different from the
-	// class variable written here. 
-	private static int rashCount = 0;		// initialize to 0
-	private static int pupilCount = 0;		// initialize to 0
-	
-	public static void main(String args[]) throws Exception {
-		
-		String currentPath = System.getProperty("user.dir");
-		String symptomPath = currentPath + "\\Project02Eclipse\\symptoms.txt" ;
-		FileReader file = new FileReader(symptomPath);
-		BufferedReader reader = new BufferedReader(file);
-		String line = reader.readLine();
-		
-		int i = 0;	// set i to 0
-		//  headaches = 0; counts headaches !! Yes, but you do have a global variable for that Purpose
-		while (line != null) {
-			i++;	// increment i
-			System.out.println("symptom n°" + i + " from file: " + line);
-			if (line.equals("headache")) {
-				headacheCount++;
-				System.out.println("number of headaches: " + headacheCount);
-			}
-			else if (line.equals("rush")) { 
-				rashCount++;
-				System.out.println("number of rush: " + rashCount);
-			}
-			else if (line.contains("pupils")) {
-				pupilCount++;
-				System.out.println("number of pupilsache: " + pupilCount);
-			}
 
-			line = reader.readLine();	// get another symptom
+	/**
+	 * Dictionary. Cannot be modified with a Setter in order to protect the data
+	 * from the Database.
+	 */
+	public Map<String, Integer> dicoOfSymptoms;
+
+	/**
+	 * Constructor of the AnalyticsCounter class.
+	 * 
+	 */
+	public AnalyticsCounter() {
+		dicoOfSymptoms = new TreeMap<String, Integer>();
+	}
+
+	public Map<String, Integer> getDicoOfSymptoms() {
+		return dicoOfSymptoms;
+	}
+
+	/**
+	 * Constructor of the AnalyticsCounter class.
+	 * 
+	 * @param ArrayList<String> a Java List with all the data.
+	 * @see ReadSymptomDataFromFile
+	 * @return the ordered dictionary with the symptom name and it's occurrences.
+	 */
+	public Map<String, Integer> generateResult(ArrayList<String> theData) {
+		for (String s : theData) {
+			if (dicoOfSymptoms.containsKey(s)) {
+				Integer newCount = dicoOfSymptoms.get(s);
+				newCount++;
+				dicoOfSymptoms.put(s, newCount);
+			} else {
+				dicoOfSymptoms.put(s, 1);
+			}
 		}
-				
-		// next generate output
-		FileWriter writer = new FileWriter ("result.out");
-		writer.write("headache: " + headacheCount + "\n");
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
+		return dicoOfSymptoms;
+	}
+
+	public static void main(String args[]) throws Exception {
+
+		String currentPath = System.getProperty("user.dir");
+		String symptomPath = currentPath + "\\Project02Eclipse\\symptoms.txt";
+		AnalyticsCounter analyticsCounter = new AnalyticsCounter();
+		ReadSymptomDataFromFile readData = new ReadSymptomDataFromFile(symptomPath);
+
+		ArrayList<String> theData = (ArrayList<String>) readData.GetSymptoms();
+
+		analyticsCounter.generateResult(theData);
+
+		FileWriter writer = new FileWriter("result.out");
+
+		for (Map.Entry<String, Integer> kv : analyticsCounter.getDicoOfSymptoms().entrySet()) {
+
+			writer.write(kv.getKey() + "=" + kv.getValue() + "\n");
+		}
+
 		writer.close();
 	}
 }
