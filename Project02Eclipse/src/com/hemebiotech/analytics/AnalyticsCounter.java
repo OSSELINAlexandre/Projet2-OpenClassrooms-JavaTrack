6b1d0d7a6ea6503bae15e129e272f90cc78cb9f0
Alexandre Osselin <alexandre.osselin@gmail.com>
2021-04-29 16:00:18 +0200

    Version 1.2

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.nio.file.FileSystems;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
@@ -71,10 +72,9 @@ public class AnalyticsCounter {
 
 	public static void main(String args[]) throws Exception {
 
-		String currentPath = System.getProperty("user.dir");
-		String symptomPath = currentPath + "\\Project02Eclipse\\symptoms.txt";
+		Path path = Paths.get("Project02Eclipse\\symptoms.txt");
 		AnalyticsCounter analyticsCounter = new AnalyticsCounter();
-		ReadSymptomDataFromFile readData = new ReadSymptomDataFromFile(symptomPath);
+		ReadSymptomDataFromFile readData = new ReadSymptomDataFromFile(path.toAbsolutePath().toString());
 
 		ArrayList<String> theData = (ArrayList<String>) readData.GetSymptoms();
 
